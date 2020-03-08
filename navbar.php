460c2de0c64f28af33e0f103fff27b9c58432f03
Tanel Tekko <taneldeviis@hotmail.com>
2020-03-08 23:15:39 +0200

    Base build

---
+++ navbar.php
@@ -0,0 +1,12 @@
+<div class='navbar'>
+    <div class='navLeft'>
+        <img src="" alt="" srcset="">
+        <ul>
+            <li>Avaleht</li>
+            <li>Kontakt</li>
+        </ul>
+    </div>
+    <div class='navright'>
+        <h1>Login</h1>
+    </div>
+</div>
